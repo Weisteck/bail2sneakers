4b6259c80d4ddced964dfd364bc0148e3376476e
Morgan <millet.morgan5@gmail.Com>
2021-03-26 16:05:50 +0100

    Ajout de bootstrap

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About'
 
 const routes = [
 	{
@@ -13,13 +14,8 @@ const routes = [
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+		component: About
 	},
-	{
-		path: '/test',
-		name: 'Test',
-		component: () => import('../views/Test')
-	}
 ]
 
 const router = createRouter({
