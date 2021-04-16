edeca8e78e78349e09daa09b03092c61e3876004
weisteck <hugo.celdran@gmail.com>
2021-04-16 17:24:16 +0200

    ⚙ init user.js model schema mongoose

---
+++ backend/src/models/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+
+const userSchema = new schema({
+  users: [
+    {
+      interest: {
+        man: { type: Boolean },
+        woman: { type: Boolean }
+      },
+      lastName: { type: String },
+      firstName: { type: String },
+      role: { type: String },
+      mail: { type: String },
+      phoneNumber: { type: String },
+      deliveryAdresses: [
+        {
+          city: { type: String },
+          adress: { type: String },
+          zipCode: { type: String },
+          country: { type: String },
+        }
+      ],
+      birthday: { type: Date },
+    }
+  ]
+})
