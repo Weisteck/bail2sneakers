80afca287a52ab1f593fcd1181be02f7c124820c
davik13 <72194058+davik13@users.noreply.github.com>
2021-04-16 12:22:15 +0200

    add mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const CONFIG = require('@yarn-monorepo/config')
-
+const cors=require('cors')
 const app = express();
+
+const connectDB=require('./config/connectDB')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('./images'))
+app.use(cors())
+
+
+//connectDB (config)
+connectDB()
 
 app.get('/', (req, res) => {
 	res.end('hello world!')
