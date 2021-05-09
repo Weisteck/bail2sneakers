cc3c864f3dcd19b87a3cba9f506cdd9c00641d8f
weisteck <hugo.celdran@gmail.com>
2021-05-09 17:32:56 +0200

    def des services post / getAll / get / put / delete et correction de la creation d un user dans repository

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,15 +1,45 @@
 const {
   postUserRepository,
   getAllUsersRepository,
-  getUserByRepository,
+  getUserByIdRepository,
   putUserRepository,
   deleteUserRepository
 } = require('../services/userRepository');
 
-const postUser = () => {
+const postUserService = async(user) => {
+  return await postUserRepository(user)
+    .then(res => res)
+    .catch(err => console.error(err))
+};
 
-}
+const getAllUsersService = async() => {
+  return await getAllUsersRepository()
+    .then(res => res)
+    .catch(err => console.error(err))
+};
 
-export {
-  postUser
+const getUserByIdService = async(id) => {
+  return await getUserByIdRepository(id)
+    .then(res => res)
+    .catch(err => console.error(err))
+};
+
+const putUserService = async(id, user) => {
+  return await putUserRepository(id, user)
+    .then(res => res)
+    .catch(err => console.error(err))
+};
+
+const deleteUserService = async(id) => {
+  return await deleteUserRepository(id)
+    .then(res => res)
+    .catch(err => console.error(err))
+};
+
+module.exports = {
+  postUserService: postUserService,
+  getAllUsersService: getAllUsersService,
+  getUserByIdService: getUserByIdService,
+  putUserService: putUserService,
+  deleteUserService: deleteUserService
 }
