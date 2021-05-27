482133b1e502ea2454a875fbae1942ecc78d39eb
Millet Morgan <millet.morgan5@gmail.com>
2021-05-27 14:54:18 +0200

    test

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,35 +1,24 @@
 <template>
 
-    <div class="container mx-auto">
-      <div class="grid grid-cols-12">
-        <div class="col-span-12 lg:col-span-8 md:col-span-6 sm:col-span-12">
-          <div class="grid grid-cols-12 col-span-12">
-            <div class="col-span-4 lg:col-span-4 md:col-span-6 sm:col-span-12 h-20 bg-red-500">
-
-            </div>
-            <div class="col-span-8 lg:col-span-4 md:col-span-6 sm:col-span-12 md:h-56 sm:h-56 h-20 bg-blue-400">
-            </div>
-          </div>
-
-        </div>
-        <div class="col-span-12 lg:col-span-4 md:col-span-6 sm:col-span-12 bg-yellow-300 text-center">
-
-          <p class="mt-5">Couleur</p>
-
-          <div class="flex mt-3 mb-3 ml-5 justify-center">
-
-          </div>
-          <div class="flex mt-3 mb-3 mx-auto w-1/2">
-            <label for="size" class="mr-10 mt-3">Taille</label>
-
-          </div>
-
-          <br>
-
-          <button @click="" class="button">
-            AJOUTER AU PANIER
-          </button>
-        </div>
-      </div>
+  <div id='demo'>
+    <Compteur v-model='resultFromChild'></Compteur>
+    This is in parent component {{ resultFromChild }}
   </div>
 </template>
+<script>
+import Compteur from "../components/Compteur.vue";
+
+export default{
+  components: {
+    Compteur,
+  },
+  data () {
+    return {
+      resultFromChild: null
+    }
+  }
+}
+
+// parent template
+
+</script>
