538c78713640256fd5f9dd5cf5291801011ab118
Millet Morgan <millet.morgan5@gmail.com>
2021-06-05 15:20:06 +0200

    add distPath

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const connectDB = require('../config/connectDB')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const distPath =
+const distPath = "../../frontend/dist/"
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
