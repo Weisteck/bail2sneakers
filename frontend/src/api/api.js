e6c6f7fc6d1da2b141162462abd8913f8eeb2e9f
Millet Morgan <millet.morgan5@gmail.com>
2021-06-12 11:09:16 +0200

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: 'http://localhost:3000/' })
+export const api = axios.create({ baseURL: `https://bail2sneakers.herokuapp.com:${process.env.PORT}/api` })
