b2d1a0ebc73a76803b3eb7d0a71fe52f06a631ff
Millet Morgan <millet.morgan5@gmail.com>
2021-06-15 11:55:32 +0200

    configuration proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default {
 	server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     }
