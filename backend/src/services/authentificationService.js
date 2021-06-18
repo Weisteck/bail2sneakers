31e081d7314d1d5addf204821efaf9fd8c7d84c2
weisteck <hugo.celdran@gmail.com>
2021-06-18 10:03:52 +0200

    debut de mise en place de passportJS, debut du fichier authentificationService ℹ️

---
+++ backend/src/services/authentificationService.js
@@ -0,0 +1,25 @@
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/users');
+
+const authentificationService = (email, password) => {
+  passport.use(new LocalStrategy(
+    (done) => {
+      User.findOne({ mail: email }, (err, user) => {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+  ));
+}
+
+module.exports = {
+  authentificationService: authentificationService,
+}
