f30c0ec58d1a94cc8c7cf70990e8bdebfa06d7be
Morgan <millet.morgan5@gmail.Com>
2021-06-18 17:36:08 +0200

    frontend

--- frontend/src/views/client/product/GetAllProducts.vue
+++ frontend/src/views/client/product/GetAllProducts.vue
@@ -2,14 +2,14 @@
 <template>
   <h1 class="text-center text-xl mt-4">Produits</h1>
   <hr>
-  <main class="container px-8 pt-24 mx-auto lg:px-4">
-    <div class="flex flex-wrap">
+  <main class="container px-8 pt-2 mx-auto lg:px-4">
+    <div class="flex flex-nowrap">
 
       <div v-for="(product) in products" :key="product.productId" class="mt-8">
 
-      <div class="rounded bg-white border-gray-200 shadow-md overflow-hidden">
-        <img :src="product.images[0]" alt="image" class=" justify-items-start object-cover w-1/4"/>
-        <p>{{ product.brand.name }}</p>
+      <div class="rounded bg-white border-gray-200 shadow-md overflow-hidden w-3/5">
+        <img :src="product.images[0]" alt="image" class=" object-contain  object-center w-full  border-b"/>
+        <p class="font-bold text-base" >{{ product.brand.name }}</p>
         <p>{{ product.model }}</p>
         <p>{{ product.priceExclTax }}</p>
         <p>
