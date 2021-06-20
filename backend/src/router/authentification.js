74575cecd89282096045c699a8e93a142703913f
Millet Morgan <millet.morgan5@gmail.com>
2021-06-20 16:54:40 +0200

    Configuration de passport

--- backend/src/router/authentification.js
+++ backend/src/router/authentification.js
@@ -1,22 +1,76 @@
 const express = require('express');
 const router = express.Router();
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require("bcrypt")
 const User = require('../models/users')
-const bcrypt = require("bcrypt");
-const session = require('express-session')
 
-router.post('/login',  async (req, res) => {
+/*router.post('/login', async (req, res) => {
   const user = await User.findOne({ "mail": req.body.username })
 
   if (user) {
     const result = await bcrypt.compare(req.body.password, user.password)
 
-    if (result)
-      res.end("password ok")
-    else
+    if (result) {
+      const newUser = {
+        id: user._id,
+        mail: user.mail
+      }
+
+      res.send(newUser)
+    } else
       res.end("wrong password")
   } else {
     res.end("utilisateur non trouvé")
   }
-});
+});*/
+
+router.post('/login', (req, res, next) => {
+
+
+  passport.authenticate('local', (err, user, info) => {
+    if (info) {
+      console.log("info", info)
+      return res.send(info.message)
+    }
+    if (err) {
+      console.log("err", err)
+      return next(err);
+    }
+    if (!user) {
+      console.log("!user")
+      return res.redirect('/login');
+    }
+    req.login(user, (err) => {
+      if (err) {
+        console.log("req.login err")
+        return next(err);
+      }
+
+      console.log("login success", user)
+      // res.send("login success")
+      return res.redirect('/api/authentification/authrequired');
+    })
+  })(req, res, next);
+})
+
+
+router.get('/authrequired', (req, res) => {
+  console.log("in authrequired", req.isAuthenticated())
+
+  if (req.isAuthenticated()) {
+    console.log("you hit the authentication endpoint\n")
+    res.send('you hit the authentication endpoint\n')
+  } else {
+    console.log("you are not connected")
+    res.send("you are not connected")
+    // res.redirect('/')
+  }
+})
+
+router.get('/login', (req, res) => {
+  res.send(`You got the login page!\n`)
+})
+
 
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,33 @@
+/**
+ * ----------------------------------------------------------------
+ *  NOTE *
+ *  ******
+ *
+ * Command for connect to /login
+ * curl http://localhost:5000/api/authentification/login -c cookie-file.txt -H 'Content-Type: application/json' -d '{"email":"test@gmail.com", "password":"trouDuCul"}' -L
+ *
+ * Problems: redirect to/api/authentification/authrequired is not working
+ *
+ *
+ * With insomnia, redirect works, but without the file cookie-file.txt, it's seem that log in not working
+ */
+
+
+
+
+
+
+
+
+
+
+
+const bcrypt = require("bcrypt");
 const comment = require("./router/comment")
-const product = require ("./router/product")
-const cart = require ("./router/cart")
+const product = require("./router/product")
+const cart = require("./router/cart")
 const user = require("./router/user")
+const authentification = require("./router/authentification")
 const express = require('express')
 const CONFIG = require('@yarn-monorepo/config')
 const cors = require('cors')
@@ -10,26 +36,81 @@ const connectDB = require('../config/connectDB')
 const { exec } = require("child_process");
 const path = require("path");
 const morgan = require("morgan")
-const authentification = require('./router/authentification')
-const bodyParser = require("body-parser")
+const passport = require("passport");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/users')
+const { v4: uuidv4 } = require('uuid');
+const FileStore = require('session-file-store')(session);
+const ObjectId = require('mongoose').Types.ObjectId;
+
 
 const app = express();
 
+// configure passport.js to use the local strategy
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  (email, password, done) => {
+    User.findOne({ "mail": email })
+      .then(response => {
+        const user = response
+        if (!user) {
+          return done(null, false, { message: 'Invalid credentials.\n' });
+        }
+        if (!bcrypt.compare(password, user.password)) {
+          return done(null, false, { message: 'Invalid credentials.\n' });
+        }
+
+        return done(null, user);
+      })
+      .catch(error => done(error));
+  }
+));
+
+// add & configure middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+  genid: (req) => {
+    return uuidv4() // use UUIDs for session IDs
+  },
+  store: new FileStore(),
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+  console.log("serializeUser", user)
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  console.log("deserializeUser, id", id)
+  User.findOne({ "_id": new ObjectId(id) })
+    .then(res => {
+      console.log("User =", res)
+      return done(null, res)
+    })
+    .catch(error => {
+      console.log("catch error", error)
+      return done(error, false)
+    })
+});
 
 connectDB()
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('./images'))
 app.use(cors({
-	origin: 'http://localhost:3001'
+  origin: 'http://localhost:3001'
 }))
 
 
@@ -45,6 +126,6 @@ app.use('/api/authentification', authentification)
 
 
 app.listen(process.env.PORT || 5000, () => {
-	console.clear()
-	console.log(`Listening on port ${ process.env.PORT || 5000 }`)
+  // console.clear()
+  console.log(`Listening on port ${ process.env.PORT || 5000 }`)
 })
