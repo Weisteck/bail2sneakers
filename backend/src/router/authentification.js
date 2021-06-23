07f7e99c53931a0d5f3a42d64ae319a2e704629c
Millet Morgan <millet.morgan5@gmail.com>
2021-06-23 20:01:33 +0200

    Debug connexion

--- backend/src/router/authentification.js
+++ backend/src/router/authentification.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const passport = require("passport")
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require("bcrypt")
+const User = require('../models/users')
 
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
@@ -8,23 +11,23 @@ router.post('/login', (req, res, next) => {
       return res.send(info.message)
 
     if (err)
-      return next(err);
+      return next(err)
 
     if (!user)
-      return res.redirect('/login');
+      return res.redirect('/login')
 
     req.login(user, (err) => {
       if (err)
-        return next(err);
+        return next(err)
 
-      return res.redirect('/api/authentification/authrequired');
+      return res.redirect('/api/authentification/authrequired')
     })
-  })(req, res, next);
+  })(req, res, next)
 })
 
 router.get('/authrequired', (req, res) => {
   req.isAuthenticated()
-    ? res.send('you hit the authentication endpoint\n')
+    ? res.send('you are connected!\n')
     : res.send("you are not connected")
 })
 
@@ -32,4 +35,4 @@ router.get('/login', (req, res) => {
   res.send(`You got the login page!\n`)
 })
 
-module.exports = router;
+module.exports = router
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt")
 const comment = require("./router/comment")
 const product = require("./router/product")
 const cart = require("./router/cart")
@@ -9,19 +9,19 @@ const CONFIG = require('@yarn-monorepo/config')
 const cors = require('cors')
 const history = require('connect-history-api-fallback')
 const connectDB = require('../config/connectDB')
-const path = require("path");
+const path = require("path")
 const morgan = require("morgan")
-const passport = require("passport");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const LocalStrategy = require('passport-local').Strategy
 const User = require('./models/users')
-const { v4: uuidv4 } = require('uuid');
-const FileStore = require('session-file-store')(session);
-const ObjectId = require('mongoose').Types.ObjectId;
+const { v4: uuidv4 } = require('uuid')
+const FileStore = require('session-file-store')(session)
+const ObjectId = require('mongoose').Types.ObjectId
 
 
-const app = express();
+const app = express()
 
 // configure passport.js to use the local strategy
 passport.use(new LocalStrategy(
@@ -30,17 +30,18 @@ passport.use(new LocalStrategy(
     User.findOne({ "mail": email })
       .then(response => {
         const user = response
+
         if (!user)
-          return done(null, false, { message: 'Invalid credentials.\n' });
+          return done(null, false, { message: 'Invalid credentials.\n' })
 
         if (!bcrypt.compare(password, user.password))
-          return done(null, false, { message: 'Invalid credentials.\n' });
+          return done(null, false, { message: 'Invalid credentials.\n' })
 
-        return done(null, user);
+        return done(null, user)
       })
-      .catch(error => done(error));
+      .catch(error => done(error))
   }
-));
+))
 
 // add & configure middleware
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -54,21 +55,21 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 // tell passport how to serialize the user
-passport.serializeUser((user, done) => done(null, user._id));
+passport.serializeUser((user, done) => done(null, user._id))
 
 passport.deserializeUser((id, done) => {
   User.findOne({ "_id": new ObjectId(id) })
     .then(res => done(null, res))
     .catch(error => done(error, false))
-});
+})
 
 connectDB()
 
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('./images'))
@@ -78,10 +79,10 @@ app.use(cors({
 
 
 app.use(history())
-app.use('/', express.static(path.join(__dirname, CONFIG.distPath)));
+app.use('/', express.static(path.join(__dirname, CONFIG.distPath)))
 
 
-app.use('/api/user', user);
+app.use('/api/user', user)
 app.use('/api/comment', comment)
 app.use('/api/cart', cart)
 app.use('/api/product', product)
@@ -89,6 +90,6 @@ app.use('/api/authentification', authentification)
 
 
 app.listen(process.env.PORT || 5000, () => {
-  console.clear()
+  // console.clear()
   console.log(`Listening on port ${ process.env.PORT || 5000 }`)
 })
