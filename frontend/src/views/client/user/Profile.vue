2d0fc3ef90b603618a5fac165a20d086f20000d8
davik13 <72194058+davik13@users.noreply.github.com>
2021-07-02 01:56:36 +0200

    objet cart a revoir

--- frontend/src/views/client/user/Profile.vue
+++ frontend/src/views/client/user/Profile.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="card">
+    <h2 class="text-base text-2xl">Inormation du profil</h2>
+    <br>
+    <p>Prenom: <span>{{user.firstName}}</span> </p>
+    <p>Nom de Famille: <span>{{user.lastName}}</span> </p>
+    <p>Role: <span>{{user.role}}</span> </p>
+    
+    <p>Email: <span>{{user.mail}}</span> </p>
 
+
+
+     <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+<div class="overflow-auto">
+        <table class="w-full">
+          <thead>
+          <tr class="font-bold uppercase border-b border-gray-300">
+            <th>Date de Commande</th>
+            <th>Adresse</th>
+            <th>Produit Selectionné</th>
+          </tr>
+          </thead>
+          <tr class="text-left border-b border-gray-200 hover:bg-gray-50 transition cursor-pointer">
+            <td>{{}}</td>
+            <td>{{  }}</td>
+            <td>{{ user.lastName }}</td>
+            <td>{{ user.firstName }}</td>
+            <td>{{ user.phoneNumber }}</td>
+            <td>{{ new Date(user.birthday).toLocaleDateString() }}</td>
+            <td>{{ user.role }}</td>
+          </tr>
+        </table>
+      </div>
   </div>
-  {{ user }}
+ 
 
 
-  <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
-
+ 
 
   {{ carts }}
 </template>
